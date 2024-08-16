97894beb8d8d41b3c5336ddd8183752b3c65aa7d
Burak <200704002@st.maltepe.edu.tr>
2024-08-16 16:10:51 +0300

    Select dynamicly added to the select input

--- src/components/MUI/MUISelect.jsx
+++ src/components/MUI/MUISelect.jsx
@@ -1,37 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import axios from "axios";
+import { Menu } from "@mui/material";
+
 
 const MUISelect = () => {
 
-    const [destinationCity, setDestinationCity] = useState(null);
+    const [destinationCity, setDestinationCity] = useState("");
+    const [cities, setCities] = useState([]);
+    const [error, setError] = useState("");
 
     const handleChange = (e) => {
         setDestinationCity(e.target.value);
         alert("City has been changed => " + e.target.value)
     }
 
-    return(
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://10.127.20.37:8080/api/cities');
+                console.log(response)
+                setCities(response.data);
+                console.log(response.data);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+
+        fetchData(); //Fetching Data
+
+    }, [])
+
+    return (
         <>
             <FormControl className="mx-9">
                 <InputLabel >Nereden</InputLabel>
                 <Select
-                value={destinationCity}
-                label="Nereden"
-                onChange={handleChange}
+                    value={destinationCity}
+                    label="Nereden"
+                    onChange={handleChange}
                 >
-                    <MenuItem value={"İstanbul"}>İstanbul</MenuItem>
-                    <MenuItem value={"Ankara"}>Ankara</MenuItem>
-                    <MenuItem value={"Bursa"}>Bursa</MenuItem>
-                    <MenuItem value={"Antalya :) "}>Antalya :D </MenuItem>
+                    {cities.map(item => (
+                        <MenuItem value={item.name}>{item.name}</MenuItem>
+                    ))}
                 </Select>
 
             </FormControl>
-        
+
         </>
     )
 }
